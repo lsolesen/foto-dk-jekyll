bd386f41d98ca413a044a20f9020aabeec205181
lsolesen <lsolesen@users.noreply.github.com>
2020-07-02 07:49:00 +0000

    Github Actions - Thu Jul  2 07:49:00 UTC 2020

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,31 @@
+var store = [{
+        "title": "Canon EOS 1100D",
+        "excerpt":"Canon EOS 1100D er et god kamera, når man vil i gang med at tage billeder. Det koster omkring 2200 kroner med et standardobjektiv. Du kan fx købe kameraet på kamerashop.dk. Hvordan bruger jeg kameraet? Du tænder det oven på. Drejeskiven lige ved siden af tænd-sluk-knappen er til at indstille,...","categories": ["Udstyr"],
+        "tags": ["Canon 1100D"],
+        "url": "/udstyr/canon-eos-1100D/",
+        "teaser": null
+      },{
+        "title": "Composition",
+        "excerpt":"I dag skal I tage 10 billeder. I skal følge rådene i nedenstående artikel - og I skal tage et billede for hver af de 10 regler for komposition. Today you take 10 pictures. Follow the advice in the article below - and take a picture fore each of the...","categories": ["Komposition"],
+        "tags": ["Komposition"],
+        "url": "/komposition/komposition/",
+        "teaser": null
+      },{
+        "title": "Hvad betyder indstillingerne?",
+        "excerpt":"Her følger en kort beskrivelse af blænde, lukkertid og ISO - og hvordan de hører sammen. Blænden Hvor stort er det hul, hvor der kommer lys in i kameraet? Et lille tal giver lille dybdeskarphed. Et højt tal giver høj dybdeskarphed. Lukkertid Hvor længe lukker kameraet lys ind? Hvis du...","categories": ["Teknik"],
+        "tags": ["Blænde","Lukkertid"],
+        "url": "/teknik/cheatsheet/",
+        "teaser": null
+      },{
+        "title": "Parafrase af Danske Pressefotos 2013",
+        "excerpt":"Kig først på årets danske pressefotos fra 2013 på aaretspressefoto.dk. Kig derefter på World Press Photo på www.worldpressphoto.org. I skal vælge et billede fra en af de to udstillinger, som I vil lave en parafrase over. En parafrase bearbejder et eksisterende kunstværk, så man måske kommer til at tænke over...","categories": ["Opgave"],
+        "tags": ["Parafrase"],
+        "url": "/opgave/parafrase/",
+        "teaser": null
+      },{
+        "title": "Opgaver med Pixlr",
+        "excerpt":"Vi skal i dag arbejde med portrætter og redigering. Tag billeder Søg på nettet efter portrætter. Find nogle portrætter, du er inspireret af. Tag mindst et portræt af jer hver især, hvor portrættet skal forsøge at fortælle noget om jer Rediger billeder / Edit pics Lav en konto på pixlr.com...","categories": ["Redigering"],
+        "tags": ["Pixlr"],
+        "url": "/redigering/edit-med-pixlr/",
+        "teaser": null
+      },]
